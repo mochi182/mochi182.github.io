f1e381cd00fb6637fa5c0be2272a017f1f5c99d1
mochi182 <mochi182tv@gmail.com>
2020-05-11 03:36:38 -0500

    Inv1

--- investigacion1/assets/js/secuenciador.js
+++ investigacion1/assets/js/secuenciador.js
@@ -1,20 +1,46 @@
-function seleccionante(){
-    var seleccion = document.getElementById('seleccionador').value;
+
+var kick = new Howl({
+    src: ['./assets/sonidos/kick.wav']
+  });
+var snare = new Howl({
+    src: ['./assets/sonidos/snare.wav']
+  });
+var piano = new Howl({
+    src: ['./assets/sonidos/piano.mp3']
+  });
+
+
+$( "td.beat" ).click(function() {
+    var filaI = this.parentElement.getAttribute('id');
+    this.classList.add("on");
+    this.style.backgroundColor ="red";
+});
+
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
+
+function queSonidos(i){
+    var x = document.getElementById("BPM").value;
     var filas = document.getElementsByTagName("tr");
-    for(var i=0; i< filas.length; i++){
-        var claseFila = filas[i].getAttribute('class');
-        if (claseFila == seleccion || claseFila == "foot"){
-            filas[i].style.display = 'table-row';
-        } else{
-            filas[i].style.display = 'none';
-        }
+    if(filas[0].cells[i].className.includes('on')){
+        kick.play();
+    };
+    if(filas[1].cells[i].className.includes('on')){
+        snare.play();
+    };
+    if(filas[2].cells[i].className.includes('on')){
+        piano.play();
     };
 };
 
-$( "#seleccionador" ).change(function() {
-    seleccionante();
-});
-
-$(document).ready(function() {
-    seleccionante();
-});
+function PLAY(){ 
+    for(var i = 0; i<18; i++){
+      queSonidos(i);
+    };
+    
+};
