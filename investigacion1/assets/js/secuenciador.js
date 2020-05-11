6111cde210f58dc1c9d9577ba967c6f5c57d2fbf
mochi182 <mochi182tv@gmail.com>
2020-05-11 15:02:39 -0500

    Inv1

--- investigacion1/assets/js/secuenciador.js
+++ investigacion1/assets/js/secuenciador.js
@@ -9,6 +9,9 @@ var piano = new Howl({
     src: ['./assets/sonidos/piano.mp3']
   });
 
+var loop = true;
+var detenido = false;
+var filas = document.getElementsByTagName("tr");
 
 $( "td.beat" ).click(function() {
     var filaI = this.parentElement.getAttribute('id');
@@ -16,31 +19,60 @@ $( "td.beat" ).click(function() {
     this.style.backgroundColor ="red";
 });
 
-function sleep(milliseconds) {
-    const date = Date.now();
-    let currentDate = null;
-    do {
-      currentDate = Date.now();
-    } while (currentDate - date < milliseconds);
-  }
-
-function queSonidos(i){
-    var x = document.getElementById("BPM").value;
-    var filas = document.getElementsByTagName("tr");
+function sonidos_por_beat(i){
     if(filas[0].cells[i].className.includes('on')){
-        kick.play();
-    };
-    if(filas[1].cells[i].className.includes('on')){
-        snare.play();
-    };
-    if(filas[2].cells[i].className.includes('on')){
-        piano.play();
-    };
+        alert('yes');
+    } else{
+        alert('fuck');
+    }
 };
 
-function PLAY(){ 
-    for(var i = 0; i<18; i++){
-      queSonidos(i);
-    };
-    
+function botonLoop(){
+    if (loop){
+        loop = false;
+        alert(loop);
+    } else{
+        loop = true;
+        alert(loop);
+    }
+};
+
+function botonStop(){
+    detenido = true;
+};
+
+function botonPlay() {
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    var x = document.getElementById("BPM").value;
+
+    if(x){
+        duracion = ((16*60)/x)*1000;
+        duracion_1_beat = duracion/16;
+        var tiempo_transcurrido = 0;
+        var i = 0;
+
+        function accionPlay() {
+            sonidos_por_beat(i);
+            i += 1;
+            tiempo_transcurrido += duracion_1_beat;
+            if(detenido){
+                detenido = false;
+                return null;
+            } else{
+                if (loop){
+                    setTimeout(accionPlay, duracion_1_beat);
+                } else{
+                    if (tiempo_transcurrido >= duracion){
+                        return null;
+                    } else{
+                        setTimeout(accionPlay, duracion_1_beat);
+                    }
+                }
+            }
+        };
+        accionPlay();
+    }else{
+        alert('Ingrese un BPM');
+    }
 };
